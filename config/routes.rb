142aa53d04c631df8d8e7206e4faa45c6e2945ac
Matt <matthewprimpas@gmail.com>
2018-03-31 12:43:39 -0400

    6.6 make payment controller functional for processing payments

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+  post 'payments/create'
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :users
   resources :orders, only: [:index, :show, :create, :destroy]
   resources :products do
     resources :comments
   end
+
 
   post 'simple_pages/thank_you'
 
