12cce0a6af6487c7edfa5563becf7fa214a87e62
Matt <matthewprimpas@gmail.com>
2018-04-19 11:22:08 -0400

    6.14 make product reviews update live for all users using actioncable actionjob

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -1,9 +1,14 @@
 class ProductChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "product_channel"
+    # stream_from "product_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def listen(data)
+    stream_for data["product_id"]
+  end
+
 end
